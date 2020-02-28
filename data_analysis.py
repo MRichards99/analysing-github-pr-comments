0ed3028fd032e2489c69f79dc03ed8c3f52937ce
MRichards99 <matthew.richards@stfc.ac.uk>
2020-02-28 09:20:14 +0000

    Add functionality to find number of records in dataset

--- data_analysis.py
+++ data_analysis.py
@@ -1,24 +1,18 @@
-import codecs
-import csv
-import lzma
 import logging
+import data_prep
 
 logger = logging.getLogger('big-data-python')
 
 
-REVIEW_COMMENTS_PATH = "/home/xfu59478/big-data-work/review_comments.csv.xz"
-
 def get_num_records():
-    with lzma.open(REVIEW_COMMENTS_PATH) as archf:
-        reader = csv.DictReader(codecs.getreader("utf-8")(archf))
+    dataset = data_prep.get_data_from_csv()
 
-        num_rows = 0
-        for record in reader:
-            num_rows += 1
-            if num_rows % 50000 == 0:
-                logger.debug('Row number: {}'.format(num_rows))
-            
-
-        logger.info('Number of records: {}'.format(num_rows))
+    num_rows = 0
+    for record in dataset:
+        num_rows += 1
+        if num_rows % 1000000 == 0:
+            logger.debug('Row number: {}'.format(num_rows))
+        
+    logger.info('Number of records: {}'.format(num_rows))
     
     return num_rows
