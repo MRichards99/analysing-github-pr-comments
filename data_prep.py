44aa49048ed7292d6e6b88f261ae2d9866a8cd23
MRichards99 <matthew.richards@stfc.ac.uk>
2020-02-29 16:15:14 +0000

    Add booleans to run certain code during certain exeuctions

--- data_prep.py
+++ data_prep.py
@@ -2,23 +2,42 @@ import codecs
 import csv
 import lzma
 import logging
+import sys
 
 logger = logging.getLogger('big-data-python')
 REVIEW_COMMENTS_PATH = "/home/xfu59478/big-data-work/review_comments.csv.xz"
 
 
 def get_data_from_csv():
+    ''' Open CSV, clean data to have in code
+    '''
     with lzma.open(REVIEW_COMMENTS_PATH) as f:
-        comment_dataset = csv.DictReader(codecs.getreader("utf-8")(f))
+        base_dataset = csv.DictReader(codecs.getreader("utf-8")(f))
+        logger.debug(type(base_dataset))
 
-        logger.debug(comment_dataset.fieldnames)
 
+        logger.debug(base_dataset.fieldnames)
+
+        clean_dataset = []
         num_rows = 0
-        for record in comment_dataset:
+        for record in base_dataset:
             num_rows += 1
-            if num_rows == 20:
-                break
+            #if num_rows == 2000000:
+                #break
 
-            logger.debug(record)
+            # Retrieve required data to make a clean dataset
+            clean_dataset.append([record['CREATED_AT'], record['BODY']])
 
-    return comment_dataset
+        logger.debug('Inside clean dataset: {}'.format(clean_dataset[2143]))
+        logger.debug('Size of dataset: {} bytes'.format(sys.getsizeof(clean_dataset)))
+
+    return clean_dataset
+
+
+def data_into_sql():
+    pass
+
+def store_clean_dataset(dataset):
+    with open('/home/xfu59478/big-data-work/analysing-github-pr-comments/dataset.txt', 'wb') as file:
+        for record in dataset:
+            file.write('{}\n'.format(record).encode('utf-8'))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import logging_config
 import data_prep
 import data_analysis
+import db_conn
 
 def stop_circusd():
     ''' Subprocess is used to stop the circusd watcher as the code will be exeucted over and over
@@ -11,17 +12,34 @@ def stop_circusd():
         connection so the code can be run while I'm not around.
     '''
 
-    time.sleep(1)
     logger.info('Code has finished, stopping Circusd watcher')
     subprocess.call(['circusctl', 'stop', 'github-big-data'])
 
+
+''' Various booleans used to trigger specific pieces of code that need to be run
+    Good for running sections of the code when debugging, without having to change code in the
+    'main' function below
+'''
+CREATE_TABLE = False
+CSV_TO_PYTHON = False
 
 
 logger = logging_config.setup_logging()
 
 if __name__ == '__main__':
     logger.info('Start of new run')
-    data_prep.get_data_from_csv()
+
+    conn = db_conn.open_db_connection()
+
+    if CREATE_TABLE:
+        db_conn.create_table(conn)
+
+    if CSV_TO_PYTHON:
+        dataset = data_prep.get_data_from_csv()
+
+    # Write it out to a file
+    #data_prep.store_clean_dataset(dataset)
+    
     
 # Stop Circusctl process from repeating code
 stop_circusd()
