1f73c5a0dadfe61c746bf0aa559b03b9067614b1
MRichards99 <matthew.richards@stfc.ac.uk>
2020-02-29 16:12:07 +0000

    Add SQL connection and create table in DB

---
+++ db_conn.py
@@ -0,0 +1,36 @@
+import logging
+import json
+
+import mysql.connector
+
+logger = logging.getLogger('big-data-python')
+SQL_CONFIG_PATH = '/home/xfu59478/big-data-work/analysing-github-pr-comments/db_config.json'
+
+def open_db_connection():
+    with open(SQL_CONFIG_PATH) as config_file:
+        # Config contains: username, password, host and database
+        sql_config = json.load(config_file)
+
+    conn = mysql.connector.connect(**sql_config)
+    logger.debug(conn)
+    logger.debug(conn.database)
+
+    return conn
+
+
+def create_table(conn):
+    TABLES = {}
+    TABLES['pr_comments'] = (
+        "CREATE TABLE `pr_comments` ("
+        "  `COMMENT_ID` int NOT NULL,"
+        "  `COMMIT_ID` varchar(50) NOT NULL,"
+        "  `URL` varchar(150) NOT NULL,"
+        "  `AUTHOR` varchar(100) NOT NULL,"
+        "  `CREATED_AT` date NOT NULL,"
+        "  `BODY` varchar(500) NOT NULL)")
+
+    cursor = conn.cursor()
+
+    for name in TABLES:
+        cursor.execute(TABLES[name])
+        logger.info('Table created called: {}'.format(name))
