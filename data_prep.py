fb7e232eaab41d4884aa3e502f7786521df13bb9
MRichards99 <matthew.richards@stfc.ac.uk>
2020-03-12 09:27:17 +0000

    Remove initial CSV ret. function code

--- data_prep.py
+++ data_prep.py
@@ -9,33 +9,10 @@ logger = logging.getLogger('big-data-python')
 REVIEW_COMMENTS_PATH = "/home/xfu59478/big-data-work/review_comments.csv.xz"
 
 
-def get_data_from_csv():
-    ''' Open CSV, clean data to have in code
-    '''
-    with lzma.open(REVIEW_COMMENTS_PATH) as f:
-        base_dataset = csv.DictReader(codecs.getreader("utf-8")(f))
-        logger.debug(type(base_dataset))
-
-
-        logger.debug(base_dataset.fieldnames)
-
-        clean_dataset = []
-        num_rows = 0
-        for record in base_dataset:
-            num_rows += 1
-            #if num_rows == 2000000:
-                #break
-
-            # Retrieve required data to make a clean dataset
-            clean_dataset.append([record['CREATED_AT'], record['BODY']])
-
-        logger.debug('Inside clean dataset: {}'.format(clean_dataset[2143]))
-        logger.debug('Size of dataset: {} bytes'.format(sys.getsizeof(clean_dataset)))
-
-    return clean_dataset
-
-
 def data_into_sql(conn):
+    ''' Take dataset from CSV file and put it into SQL database.
+        This function involves cleaning the dataset
+    '''
     with lzma.open(REVIEW_COMMENTS_PATH) as f:
         dataset = csv.DictReader(codecs.getreader("utf-8")(f))
         cursor = conn.cursor()
@@ -85,7 +62,9 @@ def data_into_sql(conn):
 
 
 
-def store_clean_dataset(dataset):
+def store_dataset(dataset):
+    ''' Store dataset stored in memory to text file
+    '''
     with open('/home/xfu59478/big-data-work/analysing-github-pr-comments/dataset.txt', 'wb') as file:
         for record in dataset:
             file.write('{}\n'.format(record).encode('utf-8'))
